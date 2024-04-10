d0621558d9ce89887651856b508b2ad4ce971d65
Ramon Ribeiro Noleto <95661913249@itajai.local>
2024-04-10 08:50:23 -0300

    criação do componente header

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,9 @@
 <template>
-  <h1>Essa é minha página de home</h1>
-  <router-link to="/login">Login</router-link>
-  <br />
-  <router-link to="/dashboard">Dashboard</router-link>
+  <!-- <div class="flex">
+    <h1>Essa é minha página de home</h1>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/dashboard">Dashboard</router-link>
+  </div> -->
 </template>
 
 <script>

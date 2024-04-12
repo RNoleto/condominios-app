4583abbd400ace82acd1468dd83ac7a1cbfc3cbf
Ramon Ribeiro Noleto <95661913249@itajai.local>
2024-04-12 12:43:06 -0300

    refatoração resume.vue

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -86,70 +86,7 @@
         >Ver detalhes</a
       >
     </div>
-    <!-- card 3 -->
-    <div
-      class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5"
-    >
-      <div class="flex justify-between mb-6">
-        <div>
-          <div class="text-2xl font-semibold mb-1">15</div>
-          <div class="text-sm font-medium text-gray-400">Usuários</div>
-        </div>
-        <div class="dropdown">
-          <button
-            type="button"
-            class="dropdown-toggle text-gray-400 hover:text-gray-600"
-          >
-            <i class="ri-more-fill"></i>
-          </button>
-          <ul
-            class="dropdown-menu shadow-md shadow-black/5 z-30 hidden py-1.5 rounded-md bg-white border border-gray-100 w-full max-w-[140px]"
-            data-popper-id="popper-5"
-            data-popper-placement="bottom-end"
-            style="
-              position: absolute;
-              inset: 0px 0px auto auto;
-              margin: 0px;
-              transform: translate(-49px, 149px);
-            "
-          >
-            <li>
-              <a
-                class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50"
-                target="_blank"
-                >Profile</a
-              >
-            </li>
-            <li>
-              <a
-                class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50"
-                target="_blank"
-                >Settings</a
-              >
-            </li>
-            <li>
-              <a
-                class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50"
-                target="_blank"
-                >Logout</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-      <a
-        href=""
-        class="text-[#f84525] font-medium text-sm hover:text-red-800"
-        target="_blank"
-        >Ver detalhes</a
-      >
-    </div>
-    
   </div>
-  <!-- Segunda Linha -->
-  <div>Linha 2</div>
-  <!-- Terceira Linha -->
-  <div>Linha 3</div>
 </template>
 
 <script>
@@ -160,6 +97,8 @@ export default {
   },
   data(){
     return {
+      card:{
+      },
       customTitle: '112',
       customSubtitle: "Imoveis Totais",
       customLinkText: 'Ver detalhes personalizado',

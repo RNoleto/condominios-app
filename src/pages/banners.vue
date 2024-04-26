0aa91e72fbba937578e46df63a5b66a4d24ebb4d
Ramon Ribeiro Noleto <ramon.zap@hotmail.com>
2024-04-25 21:18:58 -0300

    ajuste responsividade da dashboard e da tela de banners

--- src/pages/banners.vue
+++ src/pages/banners.vue
@@ -1,29 +1,31 @@
 <template>
   <div>
-    <h1>Banners</h1>
-    <div class="flex justify-between gap-10 my-2">
-      <div class="flex gap-5 w-full">
-        <card v-for="(cardData, index) in cards" :key="index" class="w-full">
+    <div class="my-2">
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        <card v-for="card in cards" :key="card.id" class="w-full">
           <template #title>
-            <div>
-              <div class="text-2xl font-semibold">{{ cardData.title }}</div>
-              <div class="text-sm font-medium text-gray-400">{{ cardData.subtitle }}</div>
+            <div class="text-2xl font-semibold">
+              {{ card.title }} {{ computeBannerCount(card.title) }}
+            </div>
+            <div class="text-sm font-medium text-gray-400">
+              {{ card.subtitle }}
             </div>
           </template>
           <template #body>
-            <!-- Conteúdo personalizado do corpo -->
-            <a :href="cardData.link" class="text-[#f84525] font-medium text-sm hover:text-red-800" target="_blank">
-              {{ cardData.linkText }}
+            <a :href="card.link" class="text-[#f84525] font-medium text-sm hover:text-red-800" target="_blank">
+              {{ card.linkText }}
             </a>
           </template>
         </card>
       </div>
     </div>
+
+
     <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-4 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
       <div v-for="banner in banners" :key="banner.id" class="m-2">
         <div class="group relative rounded-lg overflow-hidden border border-gray-200 hover:border-gray-400 shadow-md">
           <img :src="banner.imageSrc"
-            class="block w-full h-72 object-cover transition duration-300 transform group-hover:scale-105"
+            class="block w-full h-32 object-cover transition duration-300 transform group-hover:scale-105"
             alt="Banner Image">
           <div
             class="flex absolute inset-0 flex flex-col justify-end p-4 bg-gray-900 bg-opacity-70 text-white transition duration-300 opacity-0 group-hover:opacity-100">
@@ -44,8 +46,6 @@
         </div>
       </div>
     </div>
-
-
   </div>
 </template>
 
@@ -60,18 +60,21 @@ export default {
     return {
       cards: [
         {
+          id: 1,
           title: "Total de Banners",
           subtitle: "Subtítulo do Cartão 1",
           link: "/gebruikers",
           linkText: "Texto do Link 1"
         },
         {
+          id: 2,
           title: "Total de Banners Ativos",
           subtitle: "Subtítulo do Cartão 2",
           link: "/gebruikers",
           linkText: "Texto do Link 2"
         },
         {
+          id: 3,
           title: "Total de Banners Inativos",
           subtitle: "Subtítulo do Cartão 3",
           link: "/gebruikers",
@@ -81,24 +84,55 @@ export default {
       banners: [
         {
           id: 1,
-          title: "Titulo Banner 1",
+          title: "Inovação",
           imageSrc: "https://images.unsplash.com/photo-1590845947670-c009801ffa74?q=80&w=2659&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           status: "Active"
         },
         {
           id: 2,
-          title: "Titulo Banner 2",
+          title: "Dinheiro",
           imageSrc: "https://plus.unsplash.com/premium_photo-1678823283560-4453bbd2a368?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           status: "Inactive"
         },
         {
           id: 3,
-          title: "Titulo Banner 3",
+          title: "Pet Friends",
           imageSrc: "https://images.unsplash.com/photo-1450778869180-41d0601e046e?q=80&w=2572&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           status: "Inactive"
+        },
+        {
+          id: 4,
+          title: "Memes",
+          imageSrc: "https://images.unsplash.com/photo-1555685812-4b943f1cb0eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBldHN8ZW58MHx8MHx8fDA%3D",
+          status: "Inactive"
+        },
+        {
+          id: 5,
+          title: "Tecnologia",
+          imageSrc: "https://images.unsplash.com/photo-1550009158-9ebf69173e03?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGVjbm9sb2d5fGVufDB8fDB8fHww",
+          status: "Active"
+        },
+        {
+          id: 6,
+          title: "Estilo de vida",
+          imageSrc: "https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmlrZXxlbnwwfHwwfHx8MA%3D%3D",
+          status: "Active"
         }
       ]
     };
+  },
+  methods: {
+    computeBannerCount(title) {
+      if (title === "Total de Banners") {
+        return this.banners.length;
+      } else if (title === "Total de Banners Ativos") {
+        return this.banners.filter(banner => banner.status === "Active").length;
+      } else if (title === "Total de Banners Inativos") {
+        return this.banners.filter(banner => banner.status === "Inactive").length;
+      } else {
+        return 0; // Retorna 0 se o título não corresponder a nenhum dos casos acima
+      }
+    }
   }
 };
 </script>

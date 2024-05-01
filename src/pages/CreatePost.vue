68eee944f7f5547419b0bc59ee1eb4192e1467d9
Ramon Ribeiro Noleto <ramon.zap@hotmail.com>
2024-05-01 17:31:45 -0300

    ajuste da página de criação de posts

--- src/pages/CreatePost.vue
+++ src/pages/CreatePost.vue
@@ -9,12 +9,16 @@
       </div>
 
       <div class="flex flex-col mb-5">
-        <label for="status" class="mb-2">Categoria do Post:</label>
+        <label for="category" class="mb-2">Categoria do Post:</label>
+        <select id="category" v-model="editedPost.category" class="border border-gray-300 rounded-md p-2 w-full">
+          <option v-for="category in categories" :key="category.id" :value="category.name">{{ category.name }}</option>
+        </select>
       </div>
 
       <div class="flex flex-col mb-5">
         <label for="content" class="mb-2">Conteúdo do Post:</label>
-        <textarea id="content" class="border border-gray-300 rounded-md p-2 w-full h-40 resize-none"></textarea>
+        <textarea id="content"
+          class="border border-gray-300 rounded-md p-2 w-full h-40 resize-none">{{ editedPost.content }}</textarea>
       </div>
 
       <div class="flex flex-row justify-between">
@@ -23,8 +27,8 @@
           <label for="saveDraft" class="text-gray-600">Salvar em Rascunho</label>
         </div>
         <div class="flex items-center">
-          <input type="checkbox" id="saveDraft" class="mr-2" />
-          <label for="saveDraft" class="text-gray-600">Publicar</label>
+          <input type="checkbox" id="publish" class="mr-2" />
+          <label for="publish" class="text-gray-600">Publicar</label>
         </div>
         <button type="submit" class="bg-blue-500 text-white font-medium py-2 px-4 rounded-md">
           Salvar
@@ -33,7 +37,10 @@
     </form>
   </div>
 </template>
+
 <script>
+import { ref } from 'vue';
+
 export default {
   data() {
     return {
@@ -45,6 +52,7 @@ export default {
           subtitle: 'Inicie na calistenia',
           status: 'Active',
           category: 'Esporte',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
         },
         {
           id: 2,
@@ -52,6 +60,7 @@ export default {
           subtitle: 'Conheça as principais ferramentas',
           status: 'Inactive',
           category: 'Tecnologia',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
         },
         {
           id: 3,
@@ -59,6 +68,7 @@ export default {
           subtitle: 'Cuidados que você deve tomar na sua casa',
           status: 'Standby',
           category: 'Saude',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
         },
         {
           id: 4,
@@ -66,7 +76,13 @@ export default {
           subtitle: 'Saíba por onde começar',
           status: 'Active',
           category: 'Tecnologia',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
         },
+      ],
+      categories: [
+        { id: 1, name: 'Tecnologia' },
+        { id: 2, name: 'Saude' },
+        { id: 3, name: 'Esporte' },
       ]
     }
   },
@@ -75,8 +91,8 @@ export default {
     this.editedPost = this.postData.find(post => post.id === postId);
   },
   methods: {
-    saveChanges() {
-
+    savePost() {
+      // Lógica para salvar as alterações no post
     }
   }
 }
